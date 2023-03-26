67bf98a3ea03aeae23ca6e9674e300933a4793c7
famid <ahsanulhoquefamid@gmail.com>
2023-03-27 00:15:46 +0600

    Test

--- resources/views/backend/product/products/create.blade.php
+++ resources/views/backend/product/products/create.blade.php
@@ -25,7 +25,7 @@
                             <label class="col-md-3 col-from-label">{{translate('Category')}} <span class="text-danger">*</span></label>
                             <div class="col-md-8">
                                 <select class="form-control aiz-selectpicker" name="category_id" id="category_id" data-live-search="true" required>
-                                    {@dd($categories)}
+                                    @dd($categories)
                                     @foreach ($categories as $category)
 
                                     <option value="{{ $category->id }}">{{ $category->getTranslation('name') }}</option>
